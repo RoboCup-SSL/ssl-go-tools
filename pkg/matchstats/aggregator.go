fe1b542d68282ae3dc25f0f48decc61d364237ea
Nicolai Ommer <nicolai.ommer@gmail.com>
2019-10-14 00:26:06 +0200

    Close files at the end

--- pkg/matchstats/aggregator.go
+++ pkg/matchstats/aggregator.go
@@ -42,7 +42,7 @@ func (a *Aggregator) WriteJson(filename string) error {
 	if err != nil {
 		return errors.Wrap(err, "Could not marshal match stats to json")
 	}
-	return nil
+	return f.Close()
 }
 
 func (a *Aggregator) WriteBin(filename string) error {
@@ -59,5 +59,5 @@ func (a *Aggregator) WriteBin(filename string) error {
 	if err != nil {
 		return errors.Wrap(err, "Could not write match stats to binary")
 	}
-	return nil
+	return f.Close()
 }
