53235003f3e8012f6164e49abde25bc631f55e7f
Nicolai Ommer <nicolai.ommer@gmail.com>
2019-10-28 20:06:48 +0100

    Migrate match-stats to new repository

--- pkg/sslproto/generateProto.sh
+++ pkg/sslproto/generateProto.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
+
+# Fail on errors
+set -e
+
+# Update to latest protobuf compiler
 go get -u github.com/golang/protobuf/protoc-gen-go
 
+# Set package name to current directory
 packageName=${PWD##*/}
-protoc --go_out=import_path="${packageName}:." ./*.proto
+
+# compile profobuf files in current directory
+protoc -I. \
+  -I${GOPATH}/src \
+  --go_out=import_path="${packageName}:." \
+  ./*.proto
