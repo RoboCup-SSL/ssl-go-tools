3aad77c24ff3a6c521d53fcc36fc653737e16807
Nicolai Ommer <nicolai.ommer@gmail.com>
2020-01-19 15:47:45 +0100

    Update default address for tracker protocol

--- cmd/ssl-vision-tracker-client/tracker-client.go
+++ cmd/ssl-vision-tracker-client/tracker-client.go
@@ -13,7 +13,7 @@ import (
 
 const maxDatagramSize = 8192
 
-var address = flag.String("address", "224.5.23.2:10009", "The multicast address of the tracking source")
+var address = flag.String("address", "224.5.23.2:10010", "The multicast address of the tracking source")
 var fullScreen = flag.Bool("fullScreen", false, "Print the formatted frame to the console, clearing the screen during print")
 var oneFrame = flag.Bool("oneFrame", false, "Print the formatted frame to the console, exit after a single frame was received")
 var noBalls = flag.Bool("noBalls", false, "Do not print balls")
