5ed019c592a429292a84f231776098c3a77a8f73
Nicolai Ommer <nicolai.ommer@gmail.com>
2020-02-19 20:28:55 +0100

    Add quality stats for ball

--- pkg/stats/detectionQuality.go
+++ pkg/stats/detectionQuality.go
@@ -10,15 +10,20 @@ import (
 	"time"
 )
 
-const maxRobotDt = time.Millisecond * 500
-const maxDuration = time.Millisecond * 100
-const minAge = time.Second * 5
+const maxRobotDt = time.Millisecond * 1000
+const maxRobotLoss = time.Millisecond * 200
+const minRobotAge = time.Second * 10
+
+const maxBallDt = time.Millisecond * 1000
+const maxBallLoss = time.Millisecond * 100
+const minBallAge = time.Second * 5
 
 type DetectionQualityProcessor struct {
 	active          bool
 	cameraData      map[uint32]*Camera
 	robotDataLosses []*DataLoss
-	file            *os.File
+	ballDataLosses  []*DataLoss
+	robotsFile      *os.File
 	PrintDataLosses bool
 
 	FrameProcessor
@@ -27,6 +32,7 @@ type DetectionQualityProcessor struct {
 type Camera struct {
 	id     uint32
 	robots map[TeamColor]map[uint32]*Robot
+	ball   *Ball
 }
 
 type TeamColor int
@@ -38,6 +44,12 @@ const (
 
 type Robot struct {
 	id                 uint32
+	lastFrameId        uint32
+	firstDetectionTime time.Time
+	lastDetectionTime  time.Time
+}
+
+type Ball struct {
 	lastFrameId        uint32
 	firstDetectionTime time.Time
 	lastDetectionTime  time.Time
@@ -55,11 +67,11 @@ type DataLoss struct {
 func (p *DetectionQualityProcessor) Init(logFile string) error {
 	p.cameraData = map[uint32]*Camera{}
 
-	f, err := os.Create(logFile + ".csv")
+	f, err := os.Create(logFile + "_robots.csv")
 	if err != nil {
 		return err
 	}
-	p.file = f
+	p.robotsFile = f
 
 	return nil
 }
@@ -67,15 +79,15 @@ func (p *DetectionQualityProcessor) Init(logFile string) error {
 func (p *DetectionQualityProcessor) Close() error {
 
 	for _, dataLoss := range p.robotDataLosses {
-		_, err := p.file.WriteString(fmt.Sprintf("%v,%v,%v,%v,%v,%v\n", dataLoss.Time.UnixNano(), dataLoss.TeamColor, dataLoss.RobotId, dataLoss.ObjectAge.Nanoseconds(), dataLoss.NumFrames, dataLoss.Duration.Nanoseconds()))
+		_, err := p.robotsFile.WriteString(fmt.Sprintf("%v,%v,%v,%v,%v,%v\n", dataLoss.Time.UnixNano(), dataLoss.TeamColor, dataLoss.RobotId, dataLoss.ObjectAge.Nanoseconds(), dataLoss.NumFrames, dataLoss.Duration.Nanoseconds()))
 		if err != nil {
 			log.Println("Could not write timing: ", err)
 			break
 		}
 	}
 
-	if p.file != nil {
-		return p.file.Close()
+	if p.robotsFile != nil {
+		return p.robotsFile.Close()
 	}
 
 	return nil
@@ -101,6 +113,9 @@ func (p *DetectionQualityProcessor) ProcessDetection(_ *persistence.Message, fra
 	}
 	if dataLoss := camera.processRobots(frame, frame.RobotsBlue, TeamBlue); dataLoss != nil {
 		p.robotDataLosses = append(p.robotDataLosses, dataLoss)
+	}
+	if dataLoss := camera.processBalls(frame); dataLoss != nil {
+		p.ballDataLosses = append(p.ballDataLosses, dataLoss)
 	}
 }
 
@@ -134,6 +149,32 @@ func (p *Camera) processRobots(frame *sslproto.SSL_DetectionFrame, robots []*ssl
 	return
 }
 
+func (p *Camera) processBalls(frame *sslproto.SSL_DetectionFrame) (dataLoss *DataLoss) {
+	dataLoss = nil
+	if len(frame.Balls) == 0 {
+		return
+	}
+	if p.ball == nil {
+		p.ball = new(Ball)
+	}
+	tSent := toTime(*frame.TSent)
+	dt := tSent.Sub(p.ball.lastDetectionTime)
+	frameDiff := *frame.FrameNumber - p.ball.lastFrameId
+	if dt > maxBallDt {
+		p.ball.firstDetectionTime = tSent
+	} else if frameDiff > 1 {
+		dataLoss = &DataLoss{
+			Duration:  dt,
+			NumFrames: frameDiff,
+			Time:      tSent,
+			ObjectAge: tSent.Sub(p.ball.firstDetectionTime),
+		}
+	}
+	p.ball.lastDetectionTime = tSent
+	p.ball.lastFrameId = *frame.FrameNumber
+	return
+}
+
 func toTime(t float64) time.Time {
 	sentSec := int64(t)
 	sentNs := int64((t - float64(sentSec)) * 1e9)
@@ -162,41 +203,73 @@ func (p *DetectionQualityProcessor) ProcessReferee(_ *persistence.Message, frame
 }
 
 func (p *DetectionQualityProcessor) String() (res string) {
-	res += p.skippedRobotFrames()
+	res += p.robotSkippedFrames()
+	res += p.ballSkippedFrames()
 	if p.PrintDataLosses {
-		res += p.dataLossOverThreshold()
+		res += p.robotDataLossOverThreshold()
+		res += p.ballDataLossOverThreshold()
 	} else {
-		res += p.dataLossOverThresholdSum()
+		res += p.robotDataLossOverThresholdSum()
+		res += p.ballDataLossOverThresholdSum()
 	}
 	return
 }
 
-func (p *DetectionQualityProcessor) dataLossOverThreshold() (res string) {
-	res += fmt.Sprintf("Data loss > %v\n", maxDuration)
+func (p *DetectionQualityProcessor) robotDataLossOverThreshold() (res string) {
+	res += fmt.Sprintf("Data loss > %v\n", maxRobotLoss)
 
 	sort.Slice(p.robotDataLosses, func(i, j int) bool {
 		return p.robotDataLosses[i].Duration < p.robotDataLosses[j].Duration
 	})
 	for _, dataLoss := range p.robotDataLosses {
-		if dataLoss.Duration > maxDuration {
+		if dataLoss.Duration > maxRobotLoss {
 			res += fmt.Sprintf("%42v | %2d %v: %4v frames, %13v (%14v old)\n", dataLoss.Time, dataLoss.RobotId, teamColorStr(dataLoss.TeamColor), dataLoss.NumFrames, dataLoss.Duration, dataLoss.ObjectAge)
 		}
 	}
 	return
 }
 
-func (p *DetectionQualityProcessor) dataLossOverThresholdSum() (res string) {
+func (p *DetectionQualityProcessor) ballDataLossOverThreshold() (res string) {
+	res += fmt.Sprintf("Data loss > %v\n", maxBallLoss)
+
+	sort.Slice(p.ballDataLosses, func(i, j int) bool {
+		return p.ballDataLosses[i].Duration < p.ballDataLosses[j].Duration
+	})
+	for _, dataLoss := range p.ballDataLosses {
+		if dataLoss.Duration > maxBallLoss {
+			res += fmt.Sprintf("%42v | ball: %4v frames, %13v (%14v old)\n", dataLoss.Time, dataLoss.NumFrames, dataLoss.Duration, dataLoss.ObjectAge)
+		}
+	}
+	return
+}
+
+func (p *DetectionQualityProcessor) robotDataLossOverThresholdSum() (res string) {
 	numOverMax := 0
 	numOverMaxAndAged := 0
 	for _, dataLoss := range p.robotDataLosses {
-		if dataLoss.Duration > maxDuration {
+		if dataLoss.Duration > maxRobotLoss {
 			numOverMax++
-			if dataLoss.ObjectAge > minAge {
+			if dataLoss.ObjectAge > minRobotAge {
 				numOverMaxAndAged++
 			}
 		}
 	}
-	res += fmt.Sprintf("Number of data losses over %v: %v, %v older than %v", maxDuration, numOverMax, numOverMaxAndAged, minAge)
+	res += fmt.Sprintf("Number of robot data losses over %v: %v, %v older than %v\n", maxRobotLoss, numOverMax, numOverMaxAndAged, minRobotAge)
+	return
+}
+
+func (p *DetectionQualityProcessor) ballDataLossOverThresholdSum() (res string) {
+	numOverMax := 0
+	numOverMaxAndAged := 0
+	for _, dataLoss := range p.ballDataLosses {
+		if dataLoss.Duration > maxBallLoss {
+			numOverMax++
+			if dataLoss.ObjectAge > minBallAge {
+				numOverMaxAndAged++
+			}
+		}
+	}
+	res += fmt.Sprintf("Number of ball data losses over %v: %v, %v older than %v\n", maxBallLoss, numOverMax, numOverMaxAndAged, minBallAge)
 	return
 }
 
@@ -209,7 +282,7 @@ func teamColorStr(teamColor TeamColor) string {
 	return ""
 }
 
-func (p *DetectionQualityProcessor) skippedRobotFrames() (res string) {
+func (p *DetectionQualityProcessor) robotSkippedFrames() (res string) {
 	frameMisses := map[int]uint32{}
 	for _, dataLoss := range p.robotDataLosses {
 		frameMisses[int(dataLoss.NumFrames)]++
@@ -226,6 +299,30 @@ func (p *DetectionQualityProcessor) skippedRobotFrames() (res string) {
 	}
 	sort.Ints(numFramesList)
 	res += fmt.Sprintf("Robots: skipped frames:\n")
+	for _, numFrames := range numFramesList {
+		res += fmt.Sprintf("%v frames: %vx\n", numFrames, frameMisses[numFrames])
+	}
+	res += fmt.Sprintf(">=%v frames: %vx\n", maxNumFrames, maxNumFramesCount)
+	return res
+}
+
+func (p *DetectionQualityProcessor) ballSkippedFrames() (res string) {
+	frameMisses := map[int]uint32{}
+	for _, dataLoss := range p.ballDataLosses {
+		frameMisses[int(dataLoss.NumFrames)]++
+	}
+	maxNumFrames := 6
+	maxNumFramesCount := uint32(0)
+	var numFramesList []int
+	for numFrames, numFramesCount := range frameMisses {
+		if numFrames >= maxNumFrames {
+			maxNumFramesCount += numFramesCount
+		} else {
+			numFramesList = append(numFramesList, numFrames)
+		}
+	}
+	sort.Ints(numFramesList)
+	res += fmt.Sprintf("Balls: skipped frames:\n")
 	for _, numFrames := range numFramesList {
 		res += fmt.Sprintf("%v frames: %vx\n", numFrames, frameMisses[numFrames])
 	}
