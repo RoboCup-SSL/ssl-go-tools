dc4910adc14426f78f48fbbfd51d602a8ec47c07
Nicolai Ommer <nicolai.ommer@gmail.com>
2021-05-04 18:37:20 +0200

    Add a log converter that generates JSON-encoded files for further processing

---
+++ cmd/ssl-log-converter/README.md
@@ -0,0 +1,29 @@
+# ssl-log-converter
+
+Convert log files to a human-readable format - a JSON stream.
+
+## Installation
+
+Use go-get to install this executable:
+
+```
+go get -u github.com/RoboCup-SSL/ssl-go-tools/cmd/ssl-log-converter
+```
+
+## Usage
+
+The binary is called `ssl-log-converter`.
+Run it with `-h` to print usage information.
+
+The output will be written to `<input-file>.txt`.
+
+## Use cases
+
+You can convert detection or geometry messages to a text file containing one message per line, encoded as JSON.
+Afterwards, you can use common Unix CLI tools and `jq`:
+```shell
+# Extract detection frames
+ssl-log-converter -extractDetection my-logfile.log
+# Select two fields, convert them to CSV and write them to a file
+jq -r '[.camera_id, .t_capture] | @csv' my-logfile.log > data.csv
+```
---
+++ cmd/ssl-log-converter/ssl-log-converter.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/RoboCup-SSL/ssl-go-tools/pkg/persistence"
+	"log"
+	"os"
+)
+
+var extractGeometry = flag.Bool("extractGeometry", false, "Extract geometry messages into a new file")
+var extractDetection = flag.Bool("extractDetection", false, "Extract detection messages into a new file")
+var indentOutput = flag.Bool("indentOutput", false, "Indent the json-formatted output")
+
+func main() {
+	flag.Usage = func() {
+		fmt.Println("Pass one or more log files and specify one or more processors with following flags:")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) == 0 {
+		log.Fatalln("Pass one or more log files")
+	}
+
+	for _, arg := range args {
+		log.Println("Processing", arg)
+		logReader, err := persistence.NewReader(arg)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		f, err := os.Create(arg + ".txt")
+		if err != nil {
+			log.Fatalln("Could not open output file:", err)
+		}
+		channel := logReader.CreateChannel()
+
+		for r := range channel {
+			if r.MessageType.Id == persistence.MessageSslVision2014 {
+				visionMsg, err := r.ParseVisionWrapper()
+				if err != nil {
+					log.Println("Could not parse vision wrapper message:", err)
+					continue
+				}
+				if *extractGeometry && visionMsg.Geometry != nil {
+					writeMessage(f, visionMsg.Geometry)
+				}
+				if *extractDetection && visionMsg.Detection != nil {
+					writeMessage(f, visionMsg.Detection)
+				}
+			}
+		}
+
+		if err := f.Close(); err != nil {
+			log.Println("Could not close file:", err)
+		}
+	}
+}
+
+func writeMessage(f *os.File, v interface{}) {
+	var b []byte
+	var err error
+	if *indentOutput {
+		b, err = json.MarshalIndent(v, "", "  ")
+	} else {
+		b, err = json.Marshal(v)
+	}
+	if err != nil {
+		log.Println("Could not marshall detection:", err)
+	} else {
+		_, err = f.Write(b)
+		check(err)
+		_, err = f.WriteString("\n")
+		check(err)
+	}
+}
+
+func check(err error) {
+	if err != nil {
+		log.Println("Unexpected error:", err)
+	}
+}
